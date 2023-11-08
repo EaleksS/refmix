7e2f8376f87d9d20e17b3be8539904a7bb118a43
Ernest <ealgenv@gmail.com>
2023-11-08 23:18:56 +0900

    фыв

--- app/src/entities/components/Service/Service.tsx
+++ app/src/entities/components/Service/Service.tsx
@@ -9,8 +9,8 @@ export const Service: FC<IData> = ({ title, bg, img }) => {
 			<Image
 				src={bg ? bg : ""}
 				alt="bg"
-				width={387}
-				height={350}
+				width={480}
+				height={468}
 				className={styles.bg}
 			/>
 
