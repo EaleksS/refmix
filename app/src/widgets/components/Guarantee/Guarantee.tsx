e5be2c405288b908aa7b2c62b8a6a1960cfa4401
Ernest <ealgenv@gmail.com>
2023-11-09 22:03:18 +0900

    asd

--- app/src/widgets/components/Guarantee/Guarantee.tsx
+++ app/src/widgets/components/Guarantee/Guarantee.tsx
@@ -4,26 +4,40 @@ import Image from "next/image";
 
 export const Guarantee: FC = () => {
 	return (
-		<section className={styles.section} >
+		<section className={styles.section}>
 			<div className={styles.left}>
-				<Image src="/images/g-left.png" alt="left" width={640} height={315} />
+				<Image
+					src="/images/g-left.png"
+					alt="left"
+					width={640}
+					height={315}
+					priority
+				/>
 				<Image
 					src="/images/finger.png"
 					alt="left"
 					width={128}
 					height={128}
 					className={styles.finger}
+					priority
 				/>
 				<h2>Викликати Майстра</h2>
 			</div>
 			<div className={styles.right}>
-				<Image src="/images/g-right.png" alt="left" width={640} height={315} />
+				<Image
+					src="/images/g-right.png"
+					alt="left"
+					width={640}
+					height={315}
+					priority
+				/>
 				<Image
 					src="/images/finger.png"
 					alt="left"
 					width={128}
 					height={128}
 					className={styles.finger}
+					priority
 				/>
 				<h2>Отримати консультацію</h2>
 			</div>
